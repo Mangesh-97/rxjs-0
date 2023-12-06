482c312c6fe3af9efe9e72adc1377c6ed6ae807d
Mangesh <mangeshdhole97@gmail.com>
2023-12-06 19:35:27 +0530

    learn concatMap operator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { DebounceTimeComponent } from './shared/components/debounce-time/debounc
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HttpClientModule} from '@angular/common/http';
+import { ConcatMapComponent } from './shared/components/concat-map/concat-map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DebounceTimeComponent
+    DebounceTimeComponent,
+    ConcatMapComponent
   ],
   imports: [
     BrowserModule,
